6b63d1cac50156f9fed5f85eb399a78151759a92
alex91tuts <alexpintea1991@gmail.com>
2024-08-05 18:58:49 +0300

    Updates

---
+++ static/js/952.e28b04e7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbudget_app=self.webpackChunkbudget_app||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.e28b04e7.chunk.js.map
